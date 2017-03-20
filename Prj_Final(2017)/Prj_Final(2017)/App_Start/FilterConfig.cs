1c97ca5837a1e944ae0dcbef4251f8befa3a25f8
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2017-03-20 13:26:55 -0400

    Projet_Initial

---
+++ Prj_Final(2017)/Prj_Final(2017)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Final_2017_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Prj_Final(2017)/Prj_Final(2017)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prj_Final_2017_.Startup))]
+namespace Prj_Final_2017_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
