d109dbe400ad809bf4a7a7e627dc099731ce3f70
Steven <201354277@collegeahuntsic.qc.ca>
2017-03-24 13:05:07 -0400

    changement du controller panier et layout.

---
+++ Prj_Final(2017)/Prj_Final(2017)/Controllers/PanierController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Final_2017_.Controllers
+{
+    public class PanierController : Controller
+    {
+        // GET: Panier
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Panier()
+        {
+            return View();
+        }
+    }
+}
