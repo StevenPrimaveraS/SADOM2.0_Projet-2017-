d6562e21c19dcaa6629a20d6fe60f7f44795a3c5
MathieuLafond <201475492@collegeahuntsic.qc.ca>
2017-03-30 12:32:42 -0400

    Ajouté les Facade vide

---
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Facade/CompteFournisseurChambreService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Prj_Final_2017_.DTO;
+
+namespace Prj_Final_2017_.Models.Facade {
+    public class CompteFournisseurChambreFacade {
+
+        public CompteFournisseurChambreFacade() {
+
+        }
+
+        public void Add(CompteFournisseurChambreDTO compteFournisseurChambreDTO) {
+
+        }
+
+        public CompteFournisseurChambreDTO Read(int IdCompteFournisseurChambre) {
+            return null;
+        }
+
+        public void Update(CompteFournisseurChambreDTO compteFournisseurChambreDTO) {
+
+        }
+
+        public void Delete(CompteFournisseurChambreDTO compteFournisseurChambreDTO) {
+
+        }
+
+        public DataSet GetAll() {
+            return null;
+        }
+    }
+}
