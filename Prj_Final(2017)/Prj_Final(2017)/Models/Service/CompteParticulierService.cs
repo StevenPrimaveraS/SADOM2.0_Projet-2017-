34aad96b8c8a9fda7baa4a19001a0554c58f906c
Dominic Leroux <201423084@collegeahuntsic.qc.ca>
2017-03-31 14:11:51 -0400

    Début de mes Services

--- Prj_Final(2017)/Prj_Final(2017)/Models/Service/CompteParticulierService.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Service/CompteParticulierService.cs
@@ -4,32 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Prj_Final_2017_.DTO;
+using Prj_Final_2017_.Models.DAO;
+using Prj_Final_2017_.Models.Exception;
 
 namespace Prj_Final_2017_.Models.Service {
     public class CompteParticulierService {
+        private CompteParticulierDAO compteParticulierDAO;
 
-        public CompteParticulierService() {
-
+        public CompteParticulierService(CompteParticulierDAO compteParticulierDAO) {
+            if (compteParticulierDAO == null)
+            {
+                throw new VoyageAhuntsicException(3000);
+            }
         }
 
         public void Add(CompteParticulierDTO compteParticulierDTO) {
-
+            if (compteParticulierDTO == null)
+            {
+                throw new VoyageAhuntsicException(3000);
+            }
+            compteParticulierDAO.Add(compteParticulierDTO);
         }
 
-        public CompteParticulierDTO Read(int IdCompteParticulier) {
-            return null;
+        public CompteParticulierDTO Read(int idCompteParticulier) {
+            return compteParticulierDAO.Read(idCompteParticulier);
         }
 
         public void Update(CompteParticulierDTO compteParticulierDTO) {
-
+            if (compteParticulierDTO == null)
+            {
+                throw new VoyageAhuntsicException(3000);
+            }
+            compteParticulierDAO.Update(compteParticulierDTO);
         }
 
         public void Delete(CompteParticulierDTO compteParticulierDTO) {
-
+            if (compteParticulierDTO == null)
+            {
+                throw new VoyageAhuntsicException(3000);
+            }
+            compteParticulierDAO.Delete(compteParticulierDTO);
         }
 
         public DataSet GetAll() {
-            return null;
+            return compteParticulierDAO.GetAll();
         }
     }
 }
