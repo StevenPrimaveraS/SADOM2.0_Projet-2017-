ae75ae287de32e771fe577141ca3fb8377fbc900
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2017-03-31 14:29:14 -0400

    Prj_Final(2017) Début des services Chambres,Forfaits et Voitures.

--- Prj_Final(2017)/Prj_Final(2017)/Models/Service/ForfaitService.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Service/ForfaitService.cs
@@ -4,32 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Prj_Final_2017_.DTO;
+using Prj_Final_2017_.Models.DAO;
+using Prj_Final_2017_.Models.Exception;
 
-namespace Prj_Final_2017_.Models.Service {
-    public class ForfaitService {
-
-        public ForfaitService() {
-
+namespace Prj_Final_2017_.Models.Service
+{
+    public class ForfaitService
+    {
+        private ForfaitDAO forfaitDAO;
+        public ForfaitService(ForfaitDAO forfaitDAO)
+        {
+            if (forfaitDAO == null)
+            {
+                throw new VoyageAhuntsicException(6789);
+            }
         }
 
-        public void Add(ForfaitDTO forfaitDTO) {
-
+        public void Add(ForfaitDTO forfaitDTO)
+        {
+            if (forfaitDTO == null)
+            {
+                throw new VoyageAhuntsicException(6789);
+            }
+            forfaitDAO.Add(forfaitDTO);
         }
 
-        public ForfaitDTO Read(int IdForfait) {
-            return null;
+        public ForfaitDTO Read(int IdForfait)
+        {
+            return forfaitDAO.Read(IdForfait); ;
         }
 
-        public void Update(ForfaitDTO forfaitDTO) {
-
+        public void Update(ForfaitDTO forfaitDTO)
+        {
+            if (forfaitDTO == null)
+            {
+                throw new VoyageAhuntsicException(6789);
+            }
+            forfaitDAO.Update(forfaitDTO);
         }
 
-        public void Delete(ForfaitDTO forfaitDTO) {
-
+        public void Delete(ForfaitDTO forfaitDTO)
+        {
+            if (forfaitDTO == null)
+            {
+                throw new VoyageAhuntsicException(6789);
+            }
+            forfaitDAO.Delete(forfaitDTO);
         }
 
-        public DataSet GetAll() {
-            return null;
+        public DataSet GetAll()
+        {
+            return forfaitDAO.GetAll();
         }
     }
 }
--- Prj_Final(2017)/Prj_Final(2017)/Models/Service/VoitureService.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Service/VoitureService.cs
@@ -4,32 +4,55 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Prj_Final_2017_.DTO;
+using Prj_Final_2017_.Models.DAO;
+using Prj_Final_2017_.Models.Exception;
 
 namespace Prj_Final_2017_.Models.Service {
     public class VoitureService {
-
-        public VoitureService() {
-
+        private VoitureDAO voitureDAO;
+        public VoitureService(VoitureDAO voitureDAO)
+        {
+            if (voitureDAO == null)
+            {
+                throw new VoyageAhuntsicException(5678);
+            }
         }
 
-        public void Add(VoitureDTO voitureDTO) {
-
+        public void Add(VoitureDTO voitureDTO)
+        {
+            if (voitureDTO == null)
+            {
+                throw new VoyageAhuntsicException(5678);
+            }
+            voitureDAO.Add(voitureDTO);
         }
 
-        public VoitureDTO Read(int IdVoiture) {
-            return null;
+        public VoitureDTO Read(int idVoiture)
+        {
+            return voitureDAO.Read(idVoiture);
         }
 
-        public void Update(VoitureDTO voitureDTO) {
-
+        public void Update(VoitureDTO voitureDTO)
+        {
+            if (voitureDTO == null)
+            {
+                throw new VoyageAhuntsicException(5678);
+            }
+            voitureDAO.Update(voitureDTO);
         }
 
-        public void Delete(VoitureDTO voitureDTO) {
-
+        public void Delete(VoitureDTO voitureDTO)
+        {
+            if (voitureDTO == null)
+            {
+                throw new VoyageAhuntsicException(5678);
+            }
+            voitureDAO.Delete(voitureDTO);
         }
 
-        public DataSet GetAll() {
-            return null;
+        public DataSet GetAll()
+        {
+            return voitureDAO.GetAll();
         }
     }
 }
