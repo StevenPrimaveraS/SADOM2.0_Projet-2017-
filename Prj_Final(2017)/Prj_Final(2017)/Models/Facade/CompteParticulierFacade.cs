63a0abb0c229c8f913a79b3c4ff914d4af1b03cb
Dominic Leroux <201423084@collegeahuntsic.qc.ca>
2017-04-03 13:05:17 -0400

    Hotel et CompteParticulier Facades

--- Prj_Final(2017)/Prj_Final(2017)/Models/Facade/CompteParticulierFacade.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Facade/CompteParticulierFacade.cs
@@ -4,32 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Prj_Final_2017_.DTO;
+using Prj_Final_2017_.Models.Service;
+using Prj_Final_2017_.Models.Exception;
 
 namespace Prj_Final_2017_.Models.Facade {
     public class CompteParticulierFacade {
 
-        public CompteParticulierFacade() {
-
+        private CompteParticulierService compteParticulierService;
+        public CompteParticulierFacade(CompteParticulierService compteParticulierService) {
+            if (compteParticulierService == null)
+            {
+                throw new VoyageAhuntsicException(1234);
+            }
+            this.compteParticulierService = compteParticulierService;
         }
 
         public void Add(CompteParticulierDTO compteParticulierDTO) {
-
+            compteParticulierService.Add(compteParticulierDTO);
         }
 
-        public CompteParticulierDTO Read(int IdCompteParticulier) {
-            return null;
+        public CompteParticulierDTO Read(int idCompteParticulier) {
+            return compteParticulierService.Read(idCompteParticulier);
         }
 
         public void Update(CompteParticulierDTO compteParticulierDTO) {
-
+            compteParticulierService.Update(compteParticulierDTO);
         }
 
         public void Delete(CompteParticulierDTO compteParticulierDTO) {
-
+            compteParticulierService.Delete(compteParticulierDTO);
         }
 
         public DataSet GetAll() {
-            return null;
+            return compteParticulierService.GetAll();
         }
     }
 }
--- Prj_Final(2017)/Prj_Final(2017)/Models/Facade/HotelFacade.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Facade/HotelFacade.cs
@@ -4,32 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using Prj_Final_2017_.DTO;
+using Prj_Final_2017_.Models.Service;
+using Prj_Final_2017_.Models.Exception;
 
 namespace Prj_Final_2017_.Models.Facade {
     public class HotelFacade {
 
-        public HotelFacade() {
+        private HotelService hotelService;
 
+        public HotelFacade(HotelService hotelService) {
+            if(hotelService == null)
+            {
+                throw new VoyageAhuntsicException(1234);
+            }
+            this.hotelService = hotelService;
         }
 
         public void Add(HotelDTO hotelDTO) {
-
+            hotelService.Add(hotelDTO);
         }
 
-        public HotelDTO Read(int IdHotel) {
-            return null;
+        public HotelDTO Read(int idHotel) {
+            return hotelService.Read(idHotel);
         }
 
         public void Update(HotelDTO hotelDTO) {
-
+            hotelService.Update(hotelDTO);
         }
 
         public void Delete(HotelDTO hotelDTO) {
-
+            hotelService.Delete(hotelDTO);
         }
 
         public DataSet GetAll() {
-            return null;
+            return hotelService.GetAll();
         }
     }
 }
