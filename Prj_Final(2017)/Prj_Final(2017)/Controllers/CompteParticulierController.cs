61c01a96f44270e59965d12c68219237455f960f
Dominic Leroux <201423084@collegeahuntsic.qc.ca>
2017-04-03 15:43:40 -0400

    creation de CompteParticulierController

---
+++ Prj_Final(2017)/Prj_Final(2017)/Controllers/CompteParticulierController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Final_2017_.Controllers
+{
+    public class CompteParticulierController : Controller
+    {
+        // GET: CompteParticulier
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CompteParticulier/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CompteParticulier/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompteParticulier/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompteParticulier/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CompteParticulier/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompteParticulier/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CompteParticulier/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
