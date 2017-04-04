0a2c15c9e4012a0b5dddcb726a5be3bfa33f8c0f
Omar <200885307@collegeahuntsic.qc.ca>
2017-04-04 15:46:20 -0400

    j'ai créer les controlleurs pour les comptesfournisseurs

---
+++ Prj_Final(2017)/Prj_Final(2017)/Controllers/CompteFournisseurSiegeController.cs
@@ -0,0 +1,34 @@
+﻿using Prj_Final_2017_.Models.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prj_Final_2017_.Controllers
+{
+    public class CompteFournisseurSiegeController : Controller
+    {
+        // GET: CompteFournisseurSiege
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return ApplicationFunctions.CompteFournisseurSiegeFacade.Add();
+        }
+        public ActionResult Read()
+        {
+            return ApplicationFunctions.CompteFournisseurSiegeFacade.Read();
+        }
+        public ActionResult Update()
+        {
+            return ApplicationFunctions.CompteFournisseurSiegeFacade.Update();
+        }
+        public ActionResult Delete()
+        {
+            return ApplicationFunctions.CompteFournisseurSiegeFacade.Delete();
+        }
+    }
+}
