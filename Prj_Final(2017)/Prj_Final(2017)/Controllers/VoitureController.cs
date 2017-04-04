1153005242daf351a9465945701bf74648cb0d73
Anthony Chan <Anthony.Chan.1@collegeahuntsic.qc.ca>
2017-04-04 15:32:07 -0400

    Prj_Final(2017) petite modification des services voitures,chambres et forfaits

--- Prj_Final(2017)/Prj_Final(2017)/Controllers/VoitureController.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Controllers/VoitureController.cs
@@ -14,9 +14,81 @@ namespace Prj_Final_2017_.Controllers
             return View();
         }
 
+
+        // GET: Voiture/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Voiture/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Voiture/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Voiture/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Voiture/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
         public ActionResult Voiture()
         {
             return View();
+        }
+        // GET: Voiture/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Voiture/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- Prj_Final(2017)/Prj_Final(2017)/Models/Service/ChambreService.cs
+++ Prj_Final(2017)/Prj_Final(2017)/Models/Service/ChambreService.cs
@@ -9,31 +9,47 @@ using Prj_Final_2017_.Models.Exception;
 
 namespace Prj_Final_2017_.Models.Service {
     public class ChambreService {
-        ChambreDAO chambreDAO;
-        public ChambreService(ChambreDAO chambreDAO) {
+        private HotelDAO hotelDAO;
+        private ChambreDAO chambreDAO;
+        private CompteFournisseurChambreDAO compteFournisseurChambreDAO;
+        public ChambreService(ChambreDAO chambreDAO, HotelDAO hotelDAO, CompteFournisseurChambreDAO compteFournisseurChambreDAO) {
             if (chambreDAO == null)
             {
                 throw new VoyageAhuntsicException(7890);
             }
+            if (compteFournisseurChambreDAO == null)
+            {
+                throw new VoyageAhuntsicException(7890);
+            }
+            if (hotelDAO == null)
+            {
+                throw new VoyageAhuntsicException(7890);
+            }
             this.chambreDAO = chambreDAO;
+            this.hotelDAO = hotelDAO;
+            this.compteFournisseurChambreDAO = compteFournisseurChambreDAO;
         }
 
         public void Add(ChambreDTO chambreDTO) {
             if (chambreDTO == null)
             {
-                throw new VoyageAhuntsicException(5678);
+                throw new VoyageAhuntsicException(7890);
             }
             chambreDAO.Add(chambreDTO);
         }
 
-        public ChambreDTO Read(int IdChambre) {
-            return chambreDAO.Read(IdChambre);
+        public ChambreDTO Read(int idChambre) {
+            if (idChambre < 1)
+            {
+                throw new VoyageAhuntsicException(7890);
+            }
+            return chambreDAO.Read(idChambre);
         }
 
         public void Update(ChambreDTO chambreDTO) {
             if (chambreDTO == null)
             {
-                throw new VoyageAhuntsicException(5678);
+                throw new VoyageAhuntsicException(7890);
             }
             chambreDAO.Update(chambreDTO);
         }
@@ -41,7 +57,7 @@ namespace Prj_Final_2017_.Models.Service {
         public void Delete(ChambreDTO chambreDTO) {
             if (chambreDTO == null)
             {
-                throw new VoyageAhuntsicException(5678);
+                throw new VoyageAhuntsicException(7890);
             }
             chambreDAO.Delete(chambreDTO);
         }
