f0463a04e3fa597286930e6493472d6b28621931
MathieuLafond <201475492@collegeahuntsic.qc.ca>
2017-04-10 13:48:01 -0400

    Panier + ReservationChambre + Classe pour gérer les dates en string

---
+++ Prj_Final(2017)/Prj_Final(2017)/Models/util/VADateHandler.cs
@@ -0,0 +1,32 @@
+﻿using Prj_Final_2017_.Models.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prj_Final_2017_.Models.util {
+    public class VADateHandler {
+        internal static List<DateTime> reservationDates(string sDatesReservation) {
+            List<DateTime> datesReservation = new List<DateTime>();
+            try {
+                string[] sDates = sDatesReservation.Split(';');
+
+                foreach (string date in sDates) {
+                    string[] infoDate = date.Split('-');
+                    int day = int.Parse(infoDate[0]);
+                    int month = int.Parse(infoDate[1]);
+                    int year = int.Parse(infoDate[2]);
+                    DateTime dateTime = new DateTime(year, month, day);
+                    datesReservation.Add(dateTime);
+                }
+            }
+            catch (FormatException e) {
+                throw new VoyageAhuntsicException(1234);
+            }
+            catch (NullReferenceException e) {
+                throw new VoyageAhuntsicException(1234);
+            }
+            return datesReservation;
+        }
+    }
+}
